ef1e114e34002e6e49877b938da191355edbd3a3
TimofeyTrofimovich <fpm.trofimovTA@bsu.by>
2022-12-04 17:11:03 +0300

    brackets

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -16,40 +16,94 @@ struct Expression {
 
 };
 
-
+int getPriority(char);
 bool math(std::stack<Expression>&, std::stack<Expression>&, Expression&);
 
 
 int main()
 {
+
+    std::cout << "Calculator\n";
+    std::cout << "Enter the expression: \n";
     double value;
     char ch;
+    bool flag = true;
     std::stack <Expression> numbers;
     std::stack <Expression> operations;
     Expression item;
 
     while (true) {
+
         ch = std::cin.peek();       //check the next symbol but not read it
         if (ch == '\n') {
             break;
         }
-        if (ch >= '0' && ch < '9') {        //for numbers
+        else if (ch == ' ') {
+            std::cin.ignore();
+            continue;
+        }
+        else if (((ch >= '0') && (ch <= '9')) || ((ch == '-') && (flag == true))) {        //for numbers
             std::cin >> value;
             item.type = '0';
             item.value = value;
             numbers.push(item);
+            flag = false;
             continue;
         }
-        if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {     //for operations
+        else if ((ch == '+') || ((ch == '-') && (flag == 0)) || (ch == '*') || (ch == '/')) {     //for operations
+            if (operations.empty()) {
+                item.type = ch;
+                item.value = 0;
+                operations.push(item);
+                std::cin.ignore();
+                continue;
+            }
+            else if (!operations.empty() && (getPriority(ch) > getPriority(operations.top().type))) {
+                item.type = ch;
+                item.value = 0;
+                operations.push(item);
+                std::cin.ignore();
+                continue;
+            }
+            else if (!operations.empty() && (getPriority(ch) <= getPriority(operations.top().type))) {
+                if (math(numbers, operations, item) == false) {
+                    system("pause");
+                    return 0;
+                }
+                continue;
+            }
+        }
+
+        else if (ch == '(') {
             item.type = ch;
             item.value = 0;
             operations.push(item);
+            std::cin.ignore();
+            continue;
         }
-        std::cin.ignore();
+        else if (ch == ')') {
+            while (operations.top().type != '(') {
+                if (math(numbers, operations, item) == false) {
+                    system("pause");
+                    return 0;
+                }
+            }
+            operations.pop();
+            std::cin.ignore();
+            continue;
+        }
+        else {
+            std::cout << "\nwrong expression\n";
+            system("pause");
+            return 0;
+        }
     }
-    if (math(numbers, operations, item) == false) {
-        system("pause");
-        return 0;
+    while (!operations.empty())
+    {
+        if (math(numbers, operations, item) == false) {
+            system("pause");
+            return 0;
+        }
     }
     std::cout << numbers.top().value << std::endl;
     return 0;
@@ -70,6 +124,7 @@ bool math(std::stack<Expression>& numbers, std::stack<Expression>& operations, E
             item.type = '0';
             item.value = result;
             numbers.push(item);
+            operations.pop();
             break;
 
         case '-':
@@ -79,6 +134,7 @@ bool math(std::stack<Expression>& numbers, std::stack<Expression>& operations, E
             item.type = '0';
             item.value = result;
             numbers.push(item);
+            operations.pop();
             break;
 
         case '*':
@@ -88,6 +144,7 @@ bool math(std::stack<Expression>& numbers, std::stack<Expression>& operations, E
             item.type = '0';
             item.value = result;
             numbers.push(item);
+            operations.pop();
             break;
 
         case '/':
@@ -101,12 +158,25 @@ bool math(std::stack<Expression>& numbers, std::stack<Expression>& operations, E
             item.type = '0';
             item.value = result;
             numbers.push(item);
+            operations.pop();
             break;
         default:
             std::cerr << "unexpected expression";
             return false;
     }
     return true;
+}
+
+int getPriority(char ch) {
+    if (ch == '+' || ch == '-') {
+        return 1;
+    }
+    else if (ch == '*' || ch == '/') {
+        return 2;
+    }
+    else {
+        return 0;
+    }
 }
 
 
