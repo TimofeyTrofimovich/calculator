8116f830f6f4e43bc7fa8c4e0bba33c2c2e18225
TimofeyTrofimovich <fpm.trofimovTA@bsu.by>
2022-12-09 21:52:45 +0300

    third base commit

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -9,6 +9,9 @@
 #include <iostream>
 #include <stack>
 
+const double PI = acos(-1);
+
+
 struct Expression {
 
     char type;      //0 for numbers, "+" for addiction operation and so on
@@ -18,6 +21,9 @@ struct Expression {
 
 int getPriority(char);
 bool math(std::stack<Expression>&, std::stack<Expression>&, Expression&);
+double cot(double);
+double Sin(double);
+double Cos(double);
 
 
 int main()
@@ -25,6 +31,7 @@ int main()
 
     std::cout << "Calculator\n";
     std::cout << "Enter the expression: \n";
+    std::cout << "use these expressions:\nsin - sinus\ncos - cosinus\ntan - tangens\ncot  - cotangens\np - 3.14\nexp(n) - e^n\n";
     double value;
     char ch;
     bool flag = true;
@@ -50,7 +57,15 @@ int main()
             flag = false;
             continue;
         }
-        else if ((ch == '+') || ((ch == '-') && (flag == 0)) || (ch == '*') || (ch == '/')) {     //for operations
+        else if (ch == 'p') {
+            item.type = '0';
+            item.value = PI;
+            numbers.push(item);
+            flag = false;
+            std::cin.ignore();
+            continue;
+        }
+        else if ((ch == '+') || ((ch == '-') && (flag == 0)) || (ch == '*') || (ch == '/') || (ch == '^')) {     //for operations
             if (operations.empty()) {
                 item.type = ch;
                 item.value = 0;
@@ -92,6 +107,39 @@ int main()
             std::cin.ignore();
             continue;
         }
+        else if (ch == 's' || ch == 'c' || ch == 't' || ch == 'e') {
+            char arr[3];
+            for (int i=0; i < 3; i++) {
+                ch = std::cin.peek();
+                arr[i] = ch;
+                std::cin.ignore();
+            }
+            if (arr[0] == 's' && arr[1] == 'i' && arr[2] == 'n') {
+                item.type = 's';
+                item.value = 0;
+                operations.push(item);
+                continue;
+            }
+            else if (arr[0] == 'c' && arr[1] == 'o' && arr[2] == 's') {
+                item.type = 'c';
+                item.value = 0;
+                operations.push(item);
+                continue;
+            }
+            else if (arr[0] == 't' && arr[1] == 'a' && arr[2] == 'n') {
+                item.type = 't';
+                item.value = 0;
+                operations.push(item);
+                continue;
+            }
+            else if (arr[0] == 'c' && arr[1] == 'o' && arr[2] == 't') {
+                item.type = 'g';
+                item.value = 0;
+                operations.push(item);
+                continue;
+            }
+        }
+
         else {
             std::cout << "\nwrong expression\n";
             system("pause");
@@ -160,6 +208,58 @@ bool math(std::stack<Expression>& numbers, std::stack<Expression>& operations, E
             numbers.push(item);
             operations.pop();
             break;
+        case '^':
+            num2 = numbers.top().value;
+            numbers.pop();
+            result = pow(num2, num1);
+            item.type = '0';
+            item.value = result;
+            numbers.push(item);
+            operations.pop();
+            break;
+        case 's':
+            result = Sin(num1);
+            item.type = '0';
+            item.value = result;
+            numbers.push(item);
+            operations.pop();
+            break;
+        case 'c':
+            result = Cos(num1);
+            item.type = '0';
+            item.value = result;
+            numbers.push(item);
+            operations.pop();
+            break;
+        case 't':
+            if (Cos(num1) == 0) {
+                std::cerr << "incorrect argument for tangens";
+                return false;
+            }
+            result = tan(num1);
+            item.type = '0';
+            item.value = result;
+            numbers.push(item);
+            operations.pop();
+            break;
+        case 'g':
+            if (Sin(num1) == 0) {
+                std::cerr << "incorrect argument for cotangens";
+                return false;
+            }
+            result = cot(num1);
+            item.type = '0';
+            item.value = result;
+            numbers.push(item);
+            operations.pop();
+            break;
+        case 'e':
+            result = exp(num1);
+            item.type = '0';
+            item.value = result;
+            numbers.push(item);
+            operations.pop();
+            break;
         default:
             std::cerr << "unexpected expression";
             return false;
@@ -174,9 +274,28 @@ int getPriority(char ch) {
     else if (ch == '*' || ch == '/') {
         return 2;
     }
+    else if (ch == '^') {
+        return 3;
+    }
+    else if (ch == 's' || ch == 'c' || ch == 't' || ch == 'e') {
+        return 4;
+    }
     else {
         return 0;
     }
+}
+
+
+double cot(double x) {
+    return (cos(x) / sin(x));
+}
+
+double Sin(double x) {
+    return (round(sin(x) * 1000000) / 1000000);
+}
+
+double Cos(double x) {
+    return (round(cos(x) * 1000000) / 1000000);
 }
 
 
